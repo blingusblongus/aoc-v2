533725f3f25c59572c607f1eef235ab0f2682a04
Nick Allen <blingusblongus@users.noreply.github.com>
2024-11-09 16:40:50 -0600

    solve 2017 6 ts (#4)

---
+++ 2017/06/typescript/src/part1/index.ts
@@ -0,0 +1,49 @@
+const MAX_CYCLES = 100000; // For dev purposes
+
+const part1 = (input: string): string | null => {
+  const blocks = input
+    .trim()
+    .split(/\s+/)
+    .map((block) => Number(block));
+
+  const configurations = new Set<string>();
+
+  let cycles = 0;
+  while (cycles <= MAX_CYCLES) {
+    const configStr = blocks.join(",");
+    if (configurations.has(configStr)) {
+      return cycles.toString();
+    }
+    configurations.add(configStr);
+    redistribute(blocks);
+    cycles++;
+  }
+
+  return null;
+};
+
+const redistribute = (blocks: number[]) => {
+  let maxVal = 0;
+  for (let block of blocks) {
+    if (block > maxVal) maxVal = block;
+  }
+
+  // Get first block in case of ties
+  const idx = blocks.indexOf(maxVal);
+
+  // Redistribute blocks
+  const blocksToDistribute = blocks[idx];
+  blocks[idx] = 0;
+
+  let idxToModify = idx + 1;
+  for (let i = blocksToDistribute; i > 0; i--) {
+    if (blocks[idxToModify] === undefined) {
+      idxToModify = 0;
+    }
+
+    blocks[idxToModify]++;
+    idxToModify++;
+  }
+};
+
+export default part1;
