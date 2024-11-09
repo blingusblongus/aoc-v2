3a5530f77b7d5731a69d958b035fa9a03422ba70
Nick Allen <rutherford.t.b@gmail.com>
2024-11-09 16:40:35 -0600

    solve 2017 6 ts

---
+++ 2017/06/typescript/src/part1/part1.test.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect } from "vitest";
+import part1 from "./index";
+import { getAnswer, readInput } from "@utils/utils.js";
+
+const inputUrl = process.cwd() + "/../_input.txt";
+
+const input = readInput(inputUrl);
+
+if (!input) {
+  expect.fail("No input");
+}
+
+console.log("Part 1:", part1(input));
+
+describe("part1", () => {
+  const answer = getAnswer(process.cwd() + "/../_answers.txt", "part1");
+
+  if (answer) {
+    it("matches known answer", () => {
+      const result = part1(input);
+      expect(result).toEqual(answer);
+    });
+  }
+
+  describe("test cases", () => {
+    type TestCase = { in: string; out: string };
+    const cases: TestCase[] = [{ in: "0 2 7 0", out: "5" }];
+
+    cases.forEach((t) => {
+      it(`for "${t.in}" returns "${t.out}"`, () => {
+        expect(part1(t.in)).toBe(t.out);
+      });
+    });
+  });
+});
