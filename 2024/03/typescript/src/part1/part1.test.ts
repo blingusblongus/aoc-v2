c27e7c03ca53bba93f0af5cd63a2504ec1535497
Nick Allen <rutherford.t.b@gmail.com>
2024-12-03 00:00:50 -0600

    solve 2024-3-ts

---
+++ 2024/03/typescript/src/part1/part1.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from "vitest";
+import part1 from "./index";
+import { getAnswer, readInput } from "@utils/utils.js";
+
+const inputUrl = process.cwd() + "/../_input.txt";
+
+const input = readInput(inputUrl);
+
+if (!input) {
+  expect.fail("No input");
+}
+
+console.log("Part 1:", part1(input));
+
+describe("part1", () => {
+  const answer = getAnswer(process.cwd() + "/../_answers.txt", "part1");
+
+  if (answer) {
+    it("matches known answer", () => {
+      const result = part1(input);
+      expect(result).toEqual(answer);
+    });
+  }
+
+  describe("test cases", () => {
+    type TestCase = { in: string; out: string };
+    const cases: TestCase[] = [
+      {
+        in: "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+        out: "161",
+      },
+    ];
+
+    cases.forEach((t) => {
+      it(`for "${t.in}" returns "${t.out}"`, () => {
+        expect(part1(t.in)).toBe(t.out);
+      });
+    });
+  });
+});
