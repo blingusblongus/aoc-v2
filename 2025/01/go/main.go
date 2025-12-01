5f0dcc9da6a06d3bbb578bdf475bca80822e6ca9
Nick Allen <rutherford.t.b@gmail.com>
2025-11-30 23:34:04 -0600

    2025-01 part one

---
+++ 2025/01/go/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("2021/01")
+}
+
+func partOne(input string) int {
+	fmt.Println("part one:")
+
+	trimmed := strings.TrimSpace(input)
+	lines := strings.Split(trimmed, "\n")
+
+	count := 0
+	current := 50
+
+	for _, line := range lines {
+		dir := rune(line[0])
+		n, err := strconv.Atoi(line[1:])
+		// fmt.Printf("line: %s\n", line)
+		// fmt.Printf("dir: %v, n: %d\n", dir, n)
+		if err != nil {
+			log.Fatalf("error parsing line: %v", line)
+		}
+
+		n = n % 100
+
+		if dir == 'R' {
+			// fmt.Println("DIR: RIGHT")
+			current = current + n
+			if current > 99 {
+				current = current - 100
+			}
+		} else {
+			current = current - n
+			if current < 0 {
+				current = current + 100
+			}
+		}
+
+		fmt.Printf("current: %d\n", current)
+		if current == 0 {
+			count = count + 1
+		}
+	}
+
+	fmt.Printf("count: %d", count)
+	return count
+}
